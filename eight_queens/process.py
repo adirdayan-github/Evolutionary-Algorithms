6d46f1c38334343aff842d3ba26175c501c62729
amitshmidov <shmidov.amit@gmail.com>
2022-02-28 10:57:05 +0200

    crossover

--- eight_queens/process.py
+++ eight_queens/process.py
@@ -24,7 +24,15 @@ def fitness_score(perm):
 
 
 def crossover(perm1, perm2):
-    pass
+    point = np.random.randint(1, perm1.shape[0] - 1)
+    child1, child2 = perm1[:point], perm2[:point]
+    child1_to_fill = perm2[point:][~np.isin(perm2[point:], child1)]
+    child2_to_fill = perm1[point:][~np.isin(perm1[point:], child2)]
+    child1 = np.concatenate((child1, child1_to_fill))
+    child2 = np.concatenate((child2, child2_to_fill))
+    child1 = np.concatenate((child1, perm2[:point][~np.isin(perm2[:point], child1)]))
+    child2 = np.concatenate((child2, perm1[:point][~np.isin(perm1[:point], child2)]))
+    return child1, child2, point
 
 
 def mutate(perm):
@@ -36,9 +44,13 @@ def mutate(perm):
 
 
 if __name__ == '__main__':
-    s = 0
-    for _ in range(10000):
-        l = [i for i in range(8)]
-        mutate_l = mutate(l)
-        s += mutate_l != l
-    print(s)
+    # s = 0
+    # for _ in range(10000):
+    #     l = [i for i in range(8)]
+    #     mutate_l = mutate(l)
+    #     s += mutate_l != l
+    # print(s)
+
+    perm1, perm2 = [1, 3, 5, 4, 2, 6, 7, 8], [4, 5, 2, 3, 6, 8, 7, 1]
+    perm1, perm2 = np.array(perm1), np.array(perm2)
+    child1, child2, point = crossover(perm1, perm2)
